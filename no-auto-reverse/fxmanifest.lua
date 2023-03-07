8bda0aadea04cf5eb388e2be0058ae72cc858209
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-03-07 06:54:57 +0000

    Initial

---
+++ no-auto-reverse/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "no-auto-reverse"
+description "A simple script that prevents the vehicle from reversing automatically after braking."
+author "Floh"
+version "1.0.0"
+
+client_scripts {
+	'main.lua'
+}
+
---
+++ no-auto-reverse/main.lua
@@ -0,0 +1,38 @@
+local braking = false
+local shouldReverse = true
+
+CreateThread(function()
+    while true do
+        local vehicle = GetVehiclePedIsIn(PlayerPedId())
+        if not vehicle then goto continue end
+
+        local speed = GetEntitySpeed(vehicle) * 2.236936 <= 0
+        if braking and speed > 0 then
+            shouldReverse = false
+        else
+            shouldReverse = true
+        end
+
+        if not shouldReverse then
+            --SetVehicleBrake(vehicle, true)
+            --SetVehicleUndriveable(vehicle, true)
+            SetVehicleMaxSpeed(vehicle, 0.001)
+        else
+            SetVehicleMaxSpeed(vehicle, 0)
+        end
+
+        ::continue::
+        Wait(500)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        if IsControlPressed(1, 72) then
+            braking = true
+        else
+            braking = false
+        end
+        Wait(0)
+    end
+end)
